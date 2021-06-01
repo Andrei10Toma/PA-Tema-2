d91b098051ce046b1484785b6cadf957aad03660
Andrei10Toma <andreiitomaa@gmail.com>
2021-06-01 19:32:16 +0300

    adrese with comments

--- adrese.cpp
+++ adrese.cpp
@@ -35,10 +35,13 @@ class Task {
 
  private:
     int n;
+    // map from id (0, n) to the person
     unordered_map<int, Person> persons;
+    // map from email to the posesor of the mail
     unordered_map<string, int> mail_person;
     set<string> temp_set_mails;
     string min_name;
+    // the representation of the graph
     vector<int> adj[NMAX];
     void read_input() {
         ifstream fin("adrese.in");
@@ -51,10 +54,14 @@ class Task {
             person.name = name;
             for (j = 0; j < k; ++j) {
                 fin >> mail;
+                // if a person with the read email already exists an edge
+                // from person i to the person that has the email will be 
+                // created (the persons with the same mails will be connected)
                 if (mail_person.find(mail) != mail_person.end()) {
                     adj[mail_person[mail]].push_back(i);
                     adj[i].push_back(mail_person[mail]);
                 } else {
+                    // the first appearance of the mail
                     mail_person.insert(make_pair(mail, i));
                     person.mails.insert(mail);
                 }
@@ -65,9 +72,13 @@ class Task {
     }
 
     void DFS_rec(int node, int *visited, int connected_comp) {
+        // mark as visited
         visited[node] = connected_comp;
+        // insert in the temporary set mails the persons mails so that they
+        // will be sorted
         temp_set_mails.insert(persons[node].mails.begin(),
             persons[node].mails.end());
+        // the minimum name between the persons with the same mail
         if (min_name.empty()) {
             min_name = persons[node].name;
         } else {
@@ -75,6 +86,7 @@ class Task {
                 min_name = persons[node].name;
             }
         }
+        // recursive DFS for the rest of the persons
         int sz_node = adj[node].size(), i;
         for (i = 0; i < sz_node; ++i) {
             if (visited[adj[node][i]] == 0) {
@@ -85,12 +97,19 @@ class Task {
 
     int DFS(set<GoodPerson, ComparePersons> &final_person) {
         int visited[n] = {};
+        // number of the connected components
         int connected_comp = 1, i;
         for (i = 0; i < n; ++i) {
             if (visited[i] == 0) {
+                // clear the data structures for another connected component
                 temp_set_mails.clear();
                 min_name.clear();
+                // DFS recursivity for the person to see the connected 
+                // components
                 DFS_rec(i, visited, connected_comp);
+                // a connected component was found
+                // insert the person with the minimum name extracted
+                // and with the created set of mails in the final solution
                 final_person.insert(GoodPerson(min_name, temp_set_mails));
                 connected_comp++;
             }
@@ -101,8 +120,11 @@ class Task {
     void adrese() {
         set<GoodPerson, ComparePersons> final_persons;
         ofstream fout("adrese.out");
+        // DFS on the created graph
         int number_size = DFS(final_persons);
+        // number of the persons
         fout << number_size << endl;
+        // iterate through the final solution and print in the output file
         set<GoodPerson, ComparePersons>::iterator iterator_person;
         set<string>::iterator iterator_mails;
         stringstream solution;
